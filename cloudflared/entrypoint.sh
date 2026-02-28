56dbe96a6bc0df5094ade0e5ab21705925bc9791
GrumpySDB <grumpy@sdbfriends.ca>
2026-02-28 17:34:51 +0000

    chore: remove unused cloudflared customization files

--- cloudflared/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Substitute environment variables in the template and output to the real config path
-envsubst < /etc/cloudflared/config.yml.template > /etc/cloudflared/config.yml
-
-# Execute the original cloudflared command pass through from docker-compose
-exec cloudflared "$@"
